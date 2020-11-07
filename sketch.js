df5d297405e4698b78330a3ba5cd79bef96c9eb5
aryan-creator <69070325+aryan-creator@users.noreply.github.com>
2020-11-07 11:41:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+//Create variables here
+var dogimage,happydogimage 
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  sound with 
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
